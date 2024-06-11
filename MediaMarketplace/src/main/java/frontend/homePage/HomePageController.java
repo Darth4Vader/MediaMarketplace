2d9c1d3723925f3176952bb4831c49143a75f0c7
itay5 <itay5@Darth_Vader_PC>
2024-06-11 23:04:04 +0300

    Started Working on home page gui

--- MediaMarketplace/src/main/java/frontend/homePage/HomePageController.java
+++ MediaMarketplace/src/main/java/frontend/homePage/HomePageController.java
@@ -12,9 +12,25 @@ import org.springframework.stereotype.Component;
 import backend.controllers.MediaProductController;
 import backend.entities.MediaProduct;
 import backend.services.MediaProductService;
+import javafx.beans.property.DoubleProperty;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 @Component
 public class HomePageController {
@@ -24,12 +40,14 @@ public class HomePageController {
 	@FXML
 	private GridPane gridPane;
 	
+	@FXML
+	private VBox mainPane;
+	
 	@Autowired
 	private MediaProductController productController;
 
 	@FXML
 	private void initialize() throws MalformedURLException {
-		//List<MediaProduct> resp = productController.getAllMediaProducts();
 		final int cols = gridPane.getColumnCount();
 		int row = 0;
 		int currentCols = 0;
@@ -39,15 +57,54 @@ public class HomePageController {
 				"file:/C:/Users/itay5/git/MediaMarketplace/MediaMarketplace/posters/tt0096895.jpg",
 				"file:/C:/Users/itay5/git/MediaMarketplace/MediaMarketplace/posters/tt0103776.jpg",
 				"file:/C:/Users/itay5/git/MediaMarketplace/MediaMarketplace/posters/tt0372784.jpg"};
-		for(String path : list) {
+		//for(RowConstraints cc : gridPane.getRowConstraints())
+			//cc.prefHeightProperty().bind(mainPane.heightProperty().multiply(0.4));
+		
+		/*while(gridPane.getRowConstraints().size() > 0){
+			gridPane.getRowConstraints().remove(0);
+		}*/
+		/*for(ColumnConstraints cc : gridPane.getColumnConstraints())
+			cc.setPercentWidth(100/5);*/
+			//cc.setPercentHeight(0.3);
+		List<MediaProduct> resp = productController.getAllMediaProducts();
+		List<String> paths = new ArrayList<>();
+		for(MediaProduct product : resp) {
+			//paths.add(new File(product.getImagePath()).toURI().toURL().toExternalForm());
+		//}
+		//list = paths.toArray(new String[paths.size()]);
+		//for(String path : list) {
+			String path = new File(product.getImagePath()).toURI().toURL().toExternalForm();
 			ImageView view = new ImageView(path);
-			gridPane.add(view, currentCols, row);
-			if(currentCols < cols)
+			view.setPreserveRatio(true);
+			//Button view = new Button();
+			//view.maxWidth(Double.MAX_VALUE);
+			//view.maxHeight(Double.MAX_VALUE);
+			BorderPane b = new BorderPane();
+			view.fitWidthProperty().bind(mainPane.widthProperty());
+			view.fitHeightProperty().bind(mainPane.heightProperty().multiply(0.4));
+			
+			//view.fitWidthProperty().bind(gridPane.getColumnConstraints().get(currentCols).prefWidthProperty());
+			//view.fitHeightProperty().bind(gridPane.getRowConstraints().get(row).prefHeightProperty());
+			b.setCenter(view);
+			Label name = new Label(product.getMediaName());
+			b.setBottom(name);
+			//b.prefWidthProperty().bind(mainPane.widthProperty());
+			//b.maxWidthProperty().bind(mainPane.heightProperty().multiply(0.4));
+			b.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, CornerRadii.EMPTY,
+		            new BorderWidths(1))));
+			gridPane.add(b, currentCols, row);
+			System.out.println(currentCols + " " + cols);
+			System.out.println("("+currentCols+","+row+")");
+			if(currentCols < cols-1)
 				currentCols++;
 			else {
+				//RowConstraints rowConst = gridPane.getRowConstraints().get(row);
+				//System.out.println(rowConst);
 				currentCols = 0;
 				row++;
-				break;
+				//RowConstraints rowConst = new RowConstraints();
+				
+				//break;
 			}
 		}
 		/*for(MediaProduct product : resp) {
